c0206852275be55c9ed88ec6bff1b975cc31b859
Thanuja111 <83346751+Thanuja111@users.noreply.github.com>
2021-06-27 18:17:46 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+   <h1>javascript and React.js</h1>
+   <p>A basic web dev react js bootcamp </p>
+   </div>
+  );
+}
+
+export default Info;
